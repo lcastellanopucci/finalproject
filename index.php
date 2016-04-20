abac1925bccb0ce8e29093401efc27b276515a5a
lcastellanopucci <lcastellanopucci@gmail.com>
2016-04-20 15:10:21 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+# finalproject
+<!doctype HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        <h1>please work</h1>
+    </body>
+</html>
